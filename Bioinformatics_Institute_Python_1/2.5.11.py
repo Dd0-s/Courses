efcc5496a67a7f396c81868a3fa612357fd8cd0d
Alexander <106015186+Dd0-s@users.noreply.github.com>
2022-08-12 02:00:41 +0300

    Delete 2.5.11.py

--- Bioinformatics_Institute_Python_1/2.5.11.py
+++
@@ -1,9 +0,0 @@
-a = [int(i) for i in input().split()]
-b = []
-if(len(a) == 1):
-    print(a[0])
-else:
-    for i in range(len(a)):
-        b.append(a[i - 1] + a[(i + 1) % len(a)])
-    for i in range(len(b)):
-        print(b[i], end=" ")
