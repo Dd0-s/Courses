4b499bffdcec4ba0c6f797ee55eb7a33551c42ea
Alexander <106015186+Dd0-s@users.noreply.github.com>
2022-08-14 12:21:08 +0300

    Create 6.3.7.cpp

---
+++ CS_Center_C++/6.3.7.cpp
@@ -0,0 +1,9 @@
+template <typename T, typename V>
+struct SameType{
+    static const bool value = false; 
+};
+
+template <typename W>
+struct SameType<W, W>{
+    static const bool value = true;
+};
